03190b472c820bab897dddc926849924140c3019
sankalp tiwari <tsankalp64@gmail.com>
2024-06-16 00:29:31 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,10 +1,32 @@
 const net = require("net");
 
+const storage = {};
+
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
     console.log("New client connected");
+    const commands = Buffer.from(data).toString().split("\r\n");
+
+    // Check if it's a SET command
+    if (commands[0] === '*3' && commands[2] === 'SET') {
+        const key = commands[4];
+        const value = commands[6];
+        store[key] = value;
+        return connection.write("+OK\r\n");
+    }
+    // Check if it's a GET command
+    else if (commands[0] === '*2' && commands[2] === 'GET') {
+        const key = commands[4];
+        const value = store[key];
+        if (value !== undefined) {
+            return connection.write(`$${value.length}\r\n${value}\r\n`);
+        } else {
+            return connection.write("$-1\r\n");
+        }
+    }
 
     // Handle data received from the client
     connection.on("data", (data) => {
