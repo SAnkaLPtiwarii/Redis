0a1ff0366de0a1497e8399801049b64b8e635cbd
sankalp tiwari <tsankalp64@gmail.com>
2024-06-16 02:48:27 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,60 +1,46 @@
 const net = require("net");
 
-const storage = {};
+const store = new Map();
 
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
-    console.log("New client connected");
     connection.on("data", (data) => {
-
         const commands = Buffer.from(data).toString().split("\r\n");
 
         // Check if it's a SET command
         if (commands[0] === '*3' && commands[2] === 'SET') {
             const key = commands[4];
             const value = commands[6];
-            store[key] = value;
+            store.set(key, value);
             return connection.write("+OK\r\n");
         }
         // Check if it's a GET command
         else if (commands[0] === '*2' && commands[2] === 'GET') {
             const key = commands[4];
-            const value = store[key];
-            if (value !== undefined) {
+            if (store.has(key)) {
+                const value = store.get(key);
                 return connection.write(`$${value.length}\r\n${value}\r\n`);
             } else {
                 return connection.write("$-1\r\n");
             }
         }
-    });
-
-    // Handle data received from the client
-    connection.on("data", (data) => {
-        const commands = Buffer.from(data).toString().split("\r\n");
-
+        // Check if it's a PING command
+        else if (commands[0] === '*1' && commands[2] === 'PING') {
+            return connection.write("+PONG\r\n");
+        }
         // Check if it's an ECHO command
-        if (commands[0] === '*2' && commands[2] === 'ECHO') {
+        else if (commands[0] === '*2' && commands[2] === 'ECHO') {
             const str = commands[4];
             const l = str.length;
             return connection.write(`$${l}\r\n${str}\r\n`);
-        }
-        // Check if it's a PING command
-        if (commands[0] === '*1' && commands[2] === 'PING') {
-            return connection.write("+PONG\r\n");
         }
         // If the command is not recognized
         else {
             return connection.write("-ERR unknown command\r\n");
         }
-
     });
-
-
-    // Start the server on port 6379
-    server.listen(6379, "127.0.0.1", () => {
-        console.log("Server started on port 6379");
-    });
+});
+// Start the server on port 6379
+server.listen(6379, "127.0.0.1", () => {
+    console.log("Server started on port 6379");
 });
