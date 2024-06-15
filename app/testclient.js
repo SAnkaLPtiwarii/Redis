13391314e07e5ffd414728e220963faf5a018194
sankalp tiwari <tsankalp64@gmail.com>
2024-06-16 03:14:51 +0530

    pass stage

--- app/testclient.js
+++ app/testclient.js
@@ -6,9 +6,10 @@ client.connect(6379, '127.0.0.1', () => {
     console.log('Connected to server');
 
     // Test PING command
-
+    client.write('*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n');
+    client.write('*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n');
     client.write('*1\r\n$4\r\nPING\r\n');
-
+    client.write('*2\r\n$4\r\nECHO\r\n$6\r\nhello!\r\n');
 });
 
 client.on('data', (data) => {
