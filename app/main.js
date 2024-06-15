ada0f91f94ca88260adbd0958db6e8d8716c8ffa
sankalp tiwari <tsankalp64@gmail.com>
2024-06-15 13:46:48 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,8 +6,8 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
     // Handle connection
-    connection.write(`+PONG\n\r`);
-    connection.write(`+PONG\n\r`);
+    connection.write(`+PONG\r\n`);
+    connection.write(`+PONG\r\n`);
 });
 
 server.listen(6379, "127.0.0.1");
