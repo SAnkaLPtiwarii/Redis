1427871eb32f58c6f23f2a14d51792569e382fd9
sankalp tiwari <tsankalp64@gmail.com>
2024-06-18 04:22:41 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 const net = require("net");
 const { argv } = require("process");
-const replicaIndex = args.indexOf("--replicaof");
 
 console.log("Logs from your program will appear here!");
 const args = process.argv.slice(2);
 const portIdx = process.argv.indexOf("--port");
+const replicaIndex = args.indexOf("--replicaof");
 const PORT = portIdx === -1 ? 6379 : parseInt(process.argv[portIdx + 1], 10);
 const isReplica = replicaIndex !== -1 && args[replicaIndex + 1] && args[replicaIndex + 2];
 const serverType = isReplica ? "slave" : "master";
